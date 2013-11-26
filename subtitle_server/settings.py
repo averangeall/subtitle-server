5260b6eca876bb300fac940ed00fa744629bf115
averangeall <averangeall@gmail.com>
2013-11-26 15:22:42 +0800

    Successfully deploy on Fankudo

--- subtitle_server/settings.py
+++ subtitle_server/settings.py
@@ -1,3 +1,6 @@
+import database
+import deploy
+
 # Django settings for subtitle_server project.
 
 DEBUG = True
@@ -8,8 +11,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-import database
 
 DATABASES = {
     'default': database.default(),
@@ -56,11 +57,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = deploy.static_root()
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = deploy.static_url()
 
 # Additional locations of static files
 STATICFILES_DIRS = (
